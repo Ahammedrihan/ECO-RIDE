97256efc15b79d146762f761a9349782d95a3cc2
Ahammed <ahammedrihancm@gmail.com>
2023-10-19 17:12:51 +0530

    first commit user-log,reg,reset

---
+++ backend/accounts/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None,**extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        user = self.model(email=self.normalize_email(email),**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email,password=None,**extra_fields):
+        user = self.create_user(email,password=password,**extra_fields)
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(max_length=255,unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    date_of_birth = models.DateField(null=True,blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name"]
+
+    def __str__(self):
+        return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, add_label):
+        return True
+
+  
+  
+
---
+++ backend/accounts/serializers.py
@@ -0,0 +1,65 @@
+from rest_framework import serializers
+from accounts.models import CustomUser
+from django.contrib.auth.hashers import check_password
+
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style = {'input_type':'password'},write_only = True)
+    class Meta:
+        model = CustomUser
+        fields = ['email','first_name','last_name','password','password2','date_of_birth']
+        extra_kwargs ={
+            'password':{'write_only':True}
+        }    
+
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+        if password != password2:
+            raise serializers.ValidationError("Password and Confirm Password Does't Match")
+        return attrs
+    
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        return CustomUser.objects.create_user(**validated_data)
+    
+
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(max_length =255)
+    class Meta:
+        model = CustomUser
+        fields = ['email','password']
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id','email','first_name','last_name','date_of_birth']
+
+class UserChangePasswordSerializer(serializers.ModelSerializer):
+    old_password = serializers.CharField(max_length =255,style = {'input_type':'password'},write_only = True)
+    new_password = serializers.CharField(max_length =255,style = {'input_type':'password'},write_only = True)
+    new_password2 = serializers.CharField(max_length =255,style = {'input_type':'password'},write_only = True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['old_password','new_password','new_password2']
+
+    def validate(self, attrs):
+
+        old_password = attrs.get('old_password')
+        new_password = attrs.get('new_password')
+        new_password2 = attrs.get('new_password2')
+        user = self.context.get('user')
+
+        if not check_password(old_password,user.password):
+            raise serializers.ValidationError("Old Password and Entered Password Doesn't Match")
+            
+        if new_password != new_password2:
+            raise serializers.ValidationError("password doesn't match")
+        user.set_password(new_password)
+        user.save()
+        return attrs
+      
